d2526653b923b590361f4e56467d22b4b970d3b0
Simone Baglioni <simone@simonebaglioni.com>
2020-03-02 15:23:00 +0100

    from a new source

--- cmd/asdd/main.go
+++ cmd/asdd/main.go
@@ -2,182 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"strconv"
 	"syscall"
 )
 
 func main() {
-	log.Println("main called")
-
-	var err error
-
-	err = syscall.Chroot("./ubuntu_fs")
-	if err != nil {
-		log.Println("error chrooting")
-		log.Println(err)
-		os.Exit(-1)
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("invalid command!")
 	}
-
-	os.Chdir("./ubuntu_fs")
-	if err != nil {
-		log.Println("error chrooting")
-		log.Println(err)
-		os.Exit(-1)
-	}
-
-	cg()
-
-	// Mount /proc inside container so that `ps` command works
-	err = syscall.Mount("proc", "proc", "proc", 0, "")
-	if err != nil {
-		log.Println("error mounting proc")
-		log.Println(err)
-		os.Exit(-1)
-	}
-
-	path, err := exec.LookPath("/bin/uname")
-	if err != nil {
-		log.Println("LookPath error:")
-		log.Println(err)
-	} else {
-		log.Println("PATH=")
-		log.Println(path)
-	}
-
-	log.Println("preparing command")
-	cmd := exec.Command("/bin/uname", "-a")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
-
-	err = cmd.Run()
-	if err != nil {
-		log.Println("error runnig command")
-		log.Println(err)
-	} else {
-		log.Printf("command run succesfully")
-	}
-
 }
 
-func run(command ...string) {
-	log.Println("Executing", command, "from run")
-	executable, err := os.Executable()
-	if err != nil {
-		log.Println("error getting current executable")
-	}
-
-	cmd := exec.Command(executable, append([]string{"child"}, command[0:]...)...)
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	err = syscall.Sethostname([]byte("container"))
-	if err != nil {
-		log.Println("error setting hostname")
-	}
-
-	must(syscall.Chroot("./ubuntu_fs"))
-
-	path, err := exec.LookPath("/bin/bash")
-	if err != nil {
-		log.Println("LookPath error:")
-		log.Println(err)
-	} else {
-		log.Println("PATH=")
-		log.Println(path)
-	}
-
-	// Cloneflags is only available in Linux
-	// CLONE_NEWUTS namespace isolates hostname
-	// CLONE_NEWPID namespace isolates processes
-	// CLONE_NEWNS namespace isolates mounts
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
-	// Run child using namespaces. The command provided will be executed inside that.
 	must(cmd.Run())
 }
 
-func child(command ...string) {
-	log.Println("Executing", command, "from child")
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 
-	// Create cgroup
-	cg()
-
-	cmd := exec.Command(command[0], command[1:]...)
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	must(syscall.Sethostname([]byte("container")))
-	must(syscall.Chroot("./ubuntu_fs"))
-	// Change directory after chroot
-	must(os.Chdir("./ubuntu_fs"))
-	// Mount /proc inside container so that `ps` command works
+	must(syscall.Chroot("/home/joegalaxy/go/bin/ubuntu_fs"))
+	must(os.Chdir("/"))
 	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-	// Mount a temporary filesystem
-	must(syscall.Mount("something", "mytemp", "tmpfs", 0, ""))
 
-	var files []string
-	root := "/"
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		files = append(files, path)
-		return nil
-	})
-	if err != nil {
-		panic(err)
-	}
-	for _, file := range files {
-		fmt.Println(file)
-	}
-	os.Exit(0)
-
-	path, err := exec.LookPath("/bin/bash")
-	if err != nil {
-		log.Println("LookPath error:")
-		log.Println(err)
-	} else {
-		log.Println("PATH=")
-		log.Println(path)
-	}
-
-	//
-	//
-	//finfo, err := os.Stat("/bin/bash")
-	//fmt.Println("FINFO")
-	//fmt.Printf("%+v\n",finfo)
 	must(cmd.Run())
-
-	// Cleanup mount
-	must(syscall.Unmount("proc", 0))
-	must(syscall.Unmount("mytemp", 0))
-}
-
-func cg() {
-	// cgroup location in Ubuntu
-	cgroups := "/sys/fs/cgroup/"
-
-	mem := filepath.Join(cgroups, "memory")
-	kontainer := filepath.Join(mem, "kontainer")
-	os.Mkdir(kontainer, 0755)
-	// Limit memory to 1mb
-	must(ioutil.WriteFile(filepath.Join(kontainer, "memory.limit_in_bytes"), []byte("999424"), 0700))
-	// Cleanup cgroup when it is not being used
-	must(ioutil.WriteFile(filepath.Join(kontainer, "notify_on_release"), []byte("1"), 0700))
-
-	pid := strconv.Itoa(os.Getpid())
-	// Apply this and any child process in this cgroup
-	must(ioutil.WriteFile(filepath.Join(kontainer, "cgroup.procs"), []byte(pid), 0700))
 }
 
 func must(err error) {
