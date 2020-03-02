1d9db71b8a6b4ce5dec6c26a22293f939a03ae76
Simone Baglioni <simone@simonebaglioni.com>
2020-03-02 18:37:46 +0100

    maremmaccia maiala bucaiola

--- cmd/asdd/main.go
+++ cmd/asdd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-ps"
 	"os"
 	"os/exec"
 	"syscall"
@@ -19,6 +20,20 @@ func main() {
 }
 
 func run() {
+	//cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	//cmd.Stdin = os.Stdin
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//
+	//
+	//err := cmd.Run()
+	//if err!=nil {
+	//	fmt.Println("error executing initial cmd")
+	//	fmt.Println(err)
+	//}
+	//
+	//os.Exit(0)
+
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -27,26 +42,95 @@ func run() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
-	must(cmd.Run())
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("error executing first run")
+		fmt.Println(err)
+	}
 }
 
 func child() {
 	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 
+	var err error
+	err = syscall.Chroot("/home/joegalaxy/go/bin/ubuntu_fs")
+	if err != nil {
+		fmt.Println("error executing chroot")
+		fmt.Println(err)
+	}
+
+	err = os.Chdir("/")
+	if err != nil {
+		fmt.Println("error executing chdir")
+		fmt.Println(err)
+	}
+
+	err = syscall.Mount("proc", "proc", "proc", 0, "")
+	if err != nil {
+		fmt.Println("error mounting proc")
+		fmt.Println(err)
+	}
+
+	processes, err := ps.Processes()
+	if err != nil {
+		fmt.Println("error getting processes")
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	for i, process := range processes {
+		fmt.Printf("Entry: %v, PID:%v, Exec:%s\n", i, process.Pid(), process.Executable())
+	}
+
+	//var files []string
+	//root := "/"
+	//err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	//	files = append(files, path)
+	//	return nil
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, file := range files {
+	//	fmt.Println(file)
+	//}
+
+	for i, arg := range os.Args {
+		fmt.Printf("arg#%v=%s\n", i, arg)
+	}
+
+	if stat, err := os.Stat(os.Args[2]); os.IsNotExist(err) {
+		fmt.Printf("File %s doesn't exist\n", os.Args[2])
+	} else {
+		fmt.Printf("File %s exists\n", os.Args[2])
+		fmt.Printf("stat:%+v\n", stat)
+	}
+
+	//dat, err := ioutil.ReadFile("/bin/bash")
+	//if err!= nil {
+	//	fmt.Println("error printing file")
+	//	fmt.Println(err)
+	//	os.Exit(-1)
+	//} else {
+	//	fmt.Print(string(dat))
+	//}
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	//cmd := exec.Command("/bin/bash", os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-
-	must(syscall.Chroot("/home/joegalaxy/go/bin/ubuntu_fs"))
-	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-
-	must(cmd.Run())
-}
-
-func must(err error) {
+	err = cmd.Run()
 	if err != nil {
-		panic(err)
+		fmt.Println("error executing command")
+		fmt.Println(err)
+	}
+
+	os.StartProcess()
+
+	err = syscall.Unmount("proc", 0)
+	if err != nil {
+		fmt.Println("error unmounting proc")
+		fmt.Println(err)
 	}
 }
