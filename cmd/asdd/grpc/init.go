9cfde2eeee42bb68de42dfbbd8934bc15c27ec25
joegalaxy <github@simonebaglioni.com>
2020-03-06 00:52:45 +0100

    working INIT

--- cmd/asdd/grpc/init.go
+++ cmd/asdd/grpc/init.go
@@ -4,16 +4,23 @@ import (
 	"asd/common/api"
 	"asd/common/helpers"
 	"context"
+	"github.com/mistifyio/go-zfs"
 )
 
 func (s *Server) Init(ctx context.Context, in *api.Pool) (*api.Outcome, error) {
+	var apiOutcome api.Outcome
 	_log := helpers.InitLogs(true)
 	_log.Debugf("gRPC call: Init(%s)\n", in.Name)
-	//should
-	// create the config
-	// create the main dataset: asd
 
-	var apiOutcome api.Outcome
+	_, err := zfs.CreateFilesystem(in.Name+"/asd", nil)
+	if err != nil {
+		_log.Error("Error creating initial dataset " + in.Name + "/asd")
+		apiOutcome.Error = true
+		apiOutcome.Message = "Error creating initial dataset " + in.Name + "/asd"
+		return &apiOutcome, err
+	}
+
+	println("Created zfs volume" + in.Name + "/asd")
 
 	return &apiOutcome, nil
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module asd
 go 1.14
 
 require (
-	github.com/ardanlabs/conf v1.2.1
 	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/mux v1.7.4
 	github.com/lib/pq v1.3.0
 	github.com/marcsauter/single v0.0.0-20191217065835-82e78df7afac
+	github.com/mistifyio/go-zfs v2.1.1+incompatible
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/pkg/errors v0.9.1
@@ -17,5 +17,4 @@ require (
 	github.com/spf13/cobra v0.0.6
 	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980
 	google.golang.org/grpc v1.27.1
-	gopkg.in/stash.v1 v1.0.0-20171203055659-1129c19e46ec
 )
