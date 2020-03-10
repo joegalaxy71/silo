83931a79a991dc249ac3a6f8924c4b6421481603
joegalaxy <github@simonebaglioni.com>
2020-03-10 03:08:27 +0100

    methods stub

---
+++ cmd/asdlet/grpc/common.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	"asd/common/api"
+	"asd/common/helpers"
+	"fmt"
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type Server struct {
+}
+
+func Init(errGrpc chan<- error) {
+
+	_log := helpers.InitLogs(true)
+	// create a listener on TCP port 7777
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		err = errors.Wrap(err, "trying to listen on tcp")
+		errGrpc <- err
+		return
+	}
+	// create a server instance
+	s := Server{}
+	// create a gRPC server object
+	grpcServer := grpc.NewServer()
+	// attach the Ping service to the server
+	api.RegisterAsdLetServer(grpcServer, &s)
+	// start the server
+	_log.Debugf("listening for grpc connections on port: 7777")
+	if err := grpcServer.Serve(lis); err != nil {
+		err = errors.Wrap(err, "failed to serve gRPC")
+		errGrpc <- err
+		return
+	}
+}
---
+++ cmd/asdlet/grpc/node.go
@@ -0,0 +1,67 @@
+package grpc
+
+import (
+	"asd/common/api"
+	"asd/common/helpers"
+	"context"
+	"errors"
+	"github.com/mistifyio/go-zfs"
+	"github.com/spf13/viper"
+)
+
+func (s *Server) NodeAdd(ctx context.Context, in *api.Node) (*api.Node, error) {
+
+	apiNode := in
+
+	_log := helpers.InitLogs(true)
+	_log.Debugf("gRPC call: NodeAdd(pool:%s)\n", apiNode.Poolname)
+
+	_, err := zfs.CreateFilesystem(in.Poolname+"/asd", nil)
+	if err != nil {
+		message := "Error creating initial dataset " + apiNode.Poolname + "/asd"
+		_log.Error(message)
+		apiNode.Outcome.Error = true
+		apiNode.Outcome.Message = message
+		return apiNode, err
+	} else {
+		message := "Created root dataset " + apiNode.Poolname + "/asd"
+		_log.Info(message)
+		apiNode.Outcome.Error = false
+		apiNode.Outcome.Message = message
+	}
+
+	viper.Set("pool", apiNode.Poolname)
+	err = viper.WriteConfig()
+	if err != nil {
+		message := "Error persisting configuration"
+		_log.Error(message)
+		apiNode.Outcome.Error = true
+		apiNode.Outcome.Message = message
+		return apiNode, err
+	}
+	return apiNode, nil
+}
+
+func (s *Server) NodeRemove(ctx context.Context, in *api.Node) (*api.Node, error) {
+
+	apiNode := in
+
+	apiNode.Outcome.Error = true
+	apiNode.Outcome.Message = "Not implemented"
+
+	err := errors.New(apiNode.Outcome.Message)
+
+	return apiNode, nil
+}
+
+func (s *Server) NodePurge(ctx context.Context, in *api.Node) (*api.Node, error) {
+
+	apiNode := in
+
+	apiNode.Outcome.Error = true
+	apiNode.Outcome.Message = "Not implemented"
+
+	err := errors.New(apiNode.Outcome.Message)
+
+	return apiNode, nil
+}
